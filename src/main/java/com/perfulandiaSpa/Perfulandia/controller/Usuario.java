139ce48359a6f1de90314e47841ccbe5ed36c852
Mihokus <seba.rodriguezg@duocuc.cl>
2025-05-23 21:53:45 -0400

    creacion de la tabla usuario

---
+++ src/main/java/com/perfulandiaSpa/Perfulandia/controller/Usuario.java
@@ -0,0 +1,47 @@
+package com.perfulandiaSpa.Perfulandia.controller;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "usuario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+
+
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, length = 13, nullable = false)
+    private String run;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column (nullable = false)
+    private String correo;
+
+    @Column(nullable = false)
+    private Date fechaNacimiento;
+
+    @Column(nullable = false)
+    private boolean activo = true;
+
+
+
+
+
+
+
+}
