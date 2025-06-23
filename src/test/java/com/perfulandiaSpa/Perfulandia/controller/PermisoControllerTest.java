27aa1a137324e49fbce0d1f889fcb50c8507a395
mihokus <seba.rodriguezg@duocuc.cl>
2025-06-23 00:43:56 -0400

    test

--- src/test/java/com/perfulandiaSpa/Perfulandia/controller/PermisoControllerTest.java
+++ src/test/java/com/perfulandiaSpa/Perfulandia/controller/PermisoControllerTest.java
@@ -1,10 +1,34 @@
 package com.perfulandiaSpa.Perfulandia.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.perfulandiaSpa.Perfulandia.model.Permiso;
+import com.perfulandiaSpa.Perfulandia.service.PermisoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+@WebMvcTest(PermisoController.class)
 @SpringBootTest
-class PermisoControllerTest {
+public class PermisoControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PermisoService permisoService;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    private Permiso permiso;
+
+    @BeforeEach
+    void setUp() {
+
+    }
 
     @Test
     void contextLoads() {
